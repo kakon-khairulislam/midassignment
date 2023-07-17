88878b26d717ebde9087054812bd0e9e689f4389
kakon-khairulislam <kakon190299@gmail.com>
2023-07-17 08:17:46 +0600

    mid term assignment

---
+++ MTassignment1/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MTassignment1.Models;
+using System.Data.Entity;
+
+
+namespace MTassignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        AllDataContext db = new AllDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult System()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult System(AllData A)
+        {
+
+            db.AllDatas.Add(A);
+            int a = db.SaveChanges();
+            if (a > 0)
+            {
+                //ViewBag.InsertMessage = "<script>alert('Data Inserted!')</script>";
+                TempData["InsertMessage"] = "<script>alert('Data Inserted!')</script>";
+                return RedirectToAction("Track");
+                //ModelState.Clear();
+            }
+            else
+            {
+                ViewBag.InsertMessage = "<script>alert('Data Not Inserted!')</script>";
+            }
+
+
+
+            return View();
+        }
+        public ActionResult Track()
+        {
+            var data = db.AllDatas.ToList();
+            return View(data);
+        }
+        public ActionResult Admin()
+        {
+            var data = db.AllDatas.ToList();
+            return View(data);
+        }
+        public ActionResult Edit(int id)
+        {
+            var row = db.AllDatas.Where(model => model.ID == id).FirstOrDefault();
+            return View(row);
+        }
+        [HttpPost]
+        public ActionResult Edit(AllData s)
+        {
+            db.Entry(s).State = EntityState.Modified;
+            int a = db.SaveChanges();
+            if (a > 0)
+            {
+                ViewBag.UpdateMessage = "<script>alert('Data Updated!')</script>";
+                ModelState.Clear();
+            }
+           return RedirectToAction("Admin"); ;
+        }
+        public ActionResult Delete(int id)
+        {
+            var OrderID = db.AllDatas.Where(model => model.ID == id).FirstOrDefault();
+            return View(OrderID);
+        }
+        [HttpPost]
+        public ActionResult Delete(AllData s)
+        {
+            db.Entry(s).State = EntityState.Deleted;
+            int a = db.SaveChanges();
+            if (a > 0)
+            {
+                TempData["DeleteMessage"] = "<script>alert('Data Deleted!')</script>";
+
+            }
+            else
+            {
+                ViewBag.DeleteMessage = "<script>alert('Data not deleted!')</script>";
+
+            }
+            return RedirectToAction("Admin");
+        }
+    }
+}
---
+++ MTassignment1/Models/Alldata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MTassignment1.Models
+{
+    public class AllData
+    {
+        [Key]
+        public int ID { get; set; }
+        [DisplayName("Name of the Owner")]
+
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Name of the Organization")]
+        public string Organization_Name { get; set; }
+        [DisplayName("Amount of Foods in KGs")]
+        public int Amount_of_food { get; set; }
+        [DisplayName("Collection Open Time")]
+        public string Uploading_time { get; set; }
+        [DisplayName("Enter Expiration Time")]
+        public string Expired_time { get; set; }
+
+        public string Status { get; set; }
+        [DisplayName("Distribution Place")]
+        public string Place { get; set; }
+        [DisplayName("Order Carried by")]
+        public string Employee { get; set; }
+    }
+}
